6d4cca28910a76cf535e8464e14abf0dc72f4e72
dazy1030 <dev.nodajima@gmail.com>
2023-04-20 22:58:31 +0900

    Initial Commit

---
+++ AppPlayground/AppPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPlaygroundApp.swift
+//  AppPlayground
+//
+//  Created by Naoki Odajima on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct AppPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
