b19d09c6a4928043e9fe876e8be19251d1cb6f89
dazy1030 <odajima_naoki@cyberagent.co.jp>
2025-04-18 19:33:19 +0900

    add ComposableArchitecture

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -11,15 +11,29 @@ let package = Package(
         .library(name: "HomeFeature", targets: ["HomeFeature"]),
         .library(name: "MyPageFeature", targets: ["MyPageFeature"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.19.0"),
+    ],
     targets: [
         .target(
             name: "AppFeature",
             dependencies: [
                 .target(name: "HomeFeature"),
                 .target(name: "MyPageFeature"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
             ]
         ),
-        .target(name: "HomeFeature"),
-        .target(name: "MyPageFeature"),
+        .target(
+            name: "HomeFeature",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
+        .target(
+            name: "MyPageFeature",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
     ]
 )
