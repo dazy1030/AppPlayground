56ea822cbe7ad5c74bffb035b23d92a7e4ee81c3
dazy1030 <odajima_naoki@cyberagent.co.jp>
2025-04-18 18:51:18 +0900

    Initial Commit

---
+++ AppPlayground/AppPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPlaygroundApp.swift
+//  AppPlayground
+//
+//  Created by 小田島 直樹 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPlayground
+//
+//  Created by 小田島 直樹 on 4/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
